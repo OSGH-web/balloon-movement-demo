0f3c9121894fa66e20a59e3b707153d4bcdfae82
Ben Taussig <btaussig97@gmail.com>
2025-04-02 14:25:58 -0400

    remove sample terrain generation

--- scenes/terrain/terrain.gd
+++ scenes/terrain/terrain.gd
@@ -12,7 +12,6 @@ func _ready():
 	grid_dimensions = Vector2i(level.width_in_tiles, level.height_in_tiles)
 
 	generate_border()
-	generate_sample_terrain()
 
 func generate_border():
 	for x in range(grid_dimensions.x):
@@ -26,10 +25,3 @@ func generate_border():
 		set_cell(Vector2i(0, y), 0, Vector2i(3, 4), 0)
 		# Right border
 		set_cell(Vector2i(grid_dimensions.x - 1, y), 0, Vector2i(3, 4), 0)
-
-func generate_sample_terrain():
-	for x in 13:
-		set_cell(Vector2i(x, 7), 0, Vector2i(3, 4), 0)
-
-	set_cell(Vector2i(4, 6), 0, Vector2i(3, 4), 0)
-	set_cell(Vector2i(4, 5), 0, Vector2i(3, 4), 0)
