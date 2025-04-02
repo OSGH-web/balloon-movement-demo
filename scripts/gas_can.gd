50093c814e35e8d2698a6264ed71fea0c6586d8b
Ben Taussig <btaussig97@gmail.com>
2025-04-01 23:58:54 -0400

    add gas can power up

---
+++ scripts/gas_can.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export var collect_sound: AudioStream
+@onready var audio_player = $AudioStreamPlayer
+@onready var sprite = $Sprite2D
+@onready var collision = $CollisionShape2D
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	if collect_sound:
+		audio_player.stream = collect_sound
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.FUEL += 1000
+		# Disable collision and hide sprite immediately
+		collision.set_deferred("disabled", true)
+		sprite.visible = false
+		# Play sound
+		audio_player.play()
+		# Delete after sound finishes
+		await audio_player.finished
+		queue_free()
