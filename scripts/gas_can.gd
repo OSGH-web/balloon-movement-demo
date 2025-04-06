1a53d67da8317450c782d9ec566d8c40d1f60e50
jaketaussig <jaketaussig@gmail.com>
2025-04-06 15:33:31 -0700

    Removed redundant gas_can.cd code

--- scripts/gas_can.gd
+++ scripts/gas_can.gd
@@ -1,16 +1,9 @@
 class_name GasCan
 extends Area2D
-
-@export var collect_sound: AudioStream
-@onready var audio_player = $AudioStreamPlayer
-@onready var sprite = $Sprite2D
-@onready var collision = $CollisionShape2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	body_entered.connect(_on_body_entered)
-	if collect_sound:
-		audio_player.stream = collect_sound
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
