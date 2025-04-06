f35ad2c25c6031b29279063c3e1c5a5002f9ea97
jaketaussig <jaketaussig@gmail.com>
2025-04-06 15:21:09 -0700

    Implemented audio busses and simplified gas_can script

--- scripts/gas_can.gd
+++ scripts/gas_can.gd
@@ -5,20 +5,15 @@ extends Area2D
 @onready var audio_player = $AudioStreamPlayer
 @onready var sprite = $Sprite2D
 @onready var collision = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	if collect_sound:
 		audio_player.stream = collect_sound
-
+		
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.add_fuel(1000)
-		# Disable collision and hide sprite immediately
-		collision.set_deferred("disabled", true)
-		sprite.visible = false
-		# Play sound
-		audio_player.play()
-		# Delete after sound finishes
-		await audio_player.finished
-		queue_free()
+		# Collision, sound, and queue_free() are all handled here. 
+		animation_player.play("pickup")
