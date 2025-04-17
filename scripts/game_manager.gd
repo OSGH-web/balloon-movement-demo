3fd0941a9680ce56eea42fba4d77ed4da0d032b8
Ben Taussig <btaussig97@gmail.com>
2025-04-17 16:42:09 -0400

    create `extraLifeFrameDelay` variable

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,6 +7,7 @@ var lives = 1
 # 10k score gives an extra life. You automatically get 1k per level, plus however much fuel you have left over. 
 var score = 0
 var extraLivesDivisor = 1
+var extraLifeFrameDelay = .5 # value in seconds. time between life increases when receiving multiple lives
 var level_files = []
 var endZoneTriggered = false
 # Show a message upon levelCompletion
@@ -57,7 +58,7 @@ func calculateScore():
 	await scoreCountDown()
 
 	while score > 10000 * extraLivesDivisor:
-		await get_tree().create_timer(.5).timeout
+		await get_tree().create_timer(extraLifeFrameDelay).timeout
 		lives += 1
 		extraLivesDivisor += 1
 		
