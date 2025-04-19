c29d2addbb060aee04f4c9558e54778df8335261
Ben Taussig <btaussig97@gmail.com>
2025-04-19 17:35:17 -0400

    add simple timer

--- scripts/UI/hud.gd
+++ scripts/UI/hud.gd
@@ -2,11 +2,25 @@ extends CanvasLayer
 
 @export var player: CharacterBody2D
 
-func _process(_delta: float) -> void:
+var time: float = 0.0;
+
+func _process(delta: float) -> void:
+	time += delta;
+
 	if not player:
 		return
-	
+
 	var fuel_amount = max(0, int(player.FUEL))
 	%Fuel.text = "Fuel: %d" % fuel_amount
 	%Lives.text = "Lives: %d" % GameManager.lives
 	%Score.text = "Score: %d" % GameManager.score
+	%Timer.text = _format_seconds(time)
+
+func _format_seconds(time : float) -> String:
+	var minutes := time / 60
+	var seconds := fmod(time, 60)
+
+	#return "%02d:%02d" % [minutes, seconds]
+	var milliseconds := fmod(time, 1) * 100
+
+	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
