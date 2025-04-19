9bd26e91c891b105528b065fc325f25dd071a576
Ben Taussig <btaussig97@gmail.com>
2025-04-19 01:37:21 -0400

    add `Background.tscn`

--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -21,16 +21,7 @@ extends Node2D
 	set(value):
 		background_color_dark = value
 		_update_background_color()
-		
-@export var x_scale := 48:
-	set(value):
-		x_scale = value
-		_update_background_color()
-		
-@export var y_scale := 48:
-	set(value):
-		y_scale = value
-		_update_background_color()
+
 		
 @export var invert_foreground_colors := false:
 	set(value):
@@ -40,8 +31,8 @@ extends Node2D
 	set(value):
 		invert_background_colors = value
 		_update_background_color()
-				
 
+var level_background: ColorRect;
 
 func _enter_tree():
 	_update_background_color()
@@ -53,32 +44,19 @@ func _ready():
 
 
 func _update_background_color():
-	if not %TextureRect:
+	%Background.dark_color = background_color_dark
+	%Background.light_color = background_color_light
+	%Background.invert_colors = invert_background_colors
+
+	if not level_background:
 		return
 		
-	if invert_background_colors:
-		%TextureRect.material.set("shader_parameter/light_color", background_color_dark)
-		%TextureRect.material.set("shader_parameter/dark_color", background_color_light)
-	else:
-		%TextureRect.material.set("shader_parameter/light_color", background_color_light)
-		%TextureRect.material.set("shader_parameter/dark_color", background_color_dark)
-	%TextureRect.material.set("shader_parameter/x_scale", x_scale)
-	%TextureRect.material.set("shader_parameter/y_scale", y_scale)
-
-
+	level_background.color = background_color_light
 	if invert_foreground_colors:
-		%Background.material.set("shader_parameter/light_color", background_color_dark)
-		%Background.material.set("shader_parameter/dark_color", background_color_light)
-	else:
-		%Background.material.set("shader_parameter/light_color", background_color_light)
-		%Background.material.set("shader_parameter/dark_color", background_color_dark)
-	%Background.material.set("shader_parameter/x_scale", x_scale)
-	%Background.material.set("shader_parameter/y_scale", y_scale)
-	%Background.size = Vector2(
-		width_in_tiles * $Terrain.tile_set.tile_size.x,
-		height_in_tiles * $Terrain.tile_set.tile_size.y
-	)
-	%Background.position = $Terrain.position
+		level_background.color = background_color_dark
+	
+	if width_in_tiles >= 80 and height_in_tiles >= 45:
+		level_background.visible = false
 
 func _update_player_camera():
 	if Engine.is_editor_hint():
@@ -87,24 +65,21 @@ func _update_player_camera():
 		$Player._setup_camera_limits(width_in_tiles * 8 * $Player.physics_scale_factor, height_in_tiles * 8 * $Player.physics_scale_factor)
 
 func _update_level_bounds():
-	if !Engine.is_editor_hint():
-		return  # Only run in the editor
-
-	# Clear existing ColorRect (if any)
-	for child in get_children():
-		if child is ColorRect:
-			child.queue_free()
-
 	# Create new bounds overlay
 	var tilemap = get_node_or_null("Terrain")
 	if !tilemap:
 		return
+	
+		# Clear existing ColorRect (if any)
+	for child in tilemap.get_children():
+		if child is ColorRect:
+			child.queue_free()
 
-	var rect = ColorRect.new()
-	rect.color = Color(1, 0, 0, 0.1)
-	rect.size = Vector2(
+	level_background = ColorRect.new()
+	level_background.color = background_color_light
+	level_background.size = Vector2(
 		width_in_tiles * tilemap.tile_set.tile_size.x,
 		height_in_tiles * tilemap.tile_set.tile_size.y
 	)
-	add_child(rect)
-	rect.z_index = 1  # Render behind tiles
+	tilemap.add_child(level_background)
+	level_background.z_index = -1  # Render behind tiles
