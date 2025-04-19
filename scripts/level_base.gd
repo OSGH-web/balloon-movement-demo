6e480c7456f7270a5f470fe0774387c849304bbd
Ben Taussig <btaussig97@gmail.com>
2025-04-19 12:56:24 -0400

    move level background functionality to `terrain.gd`

--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -21,7 +21,6 @@ extends Node2D
 	set(value):
 		background_color_dark = value
 		_update_background_color()
-
 		
 @export var invert_foreground_colors := false:
 	set(value):
@@ -31,8 +30,6 @@ extends Node2D
 	set(value):
 		invert_background_colors = value
 		_update_background_color()
-
-var level_background: ColorRect;
 
 func _enter_tree():
 	_update_background_color()
@@ -47,16 +44,13 @@ func _update_background_color():
 	%Background.dark_color = background_color_dark
 	%Background.light_color = background_color_light
 	%Background.invert_colors = invert_background_colors
-
-	if not level_background:
-		return
 		
-	level_background.color = background_color_light
+	$Terrain.background_color = background_color_light
 	if invert_foreground_colors:
-		level_background.color = background_color_dark
+		$Terrain.background_color = background_color_dark
 	
 	if width_in_tiles >= 80 and height_in_tiles >= 45:
-		level_background.visible = false
+		$Terrain.background_color = Color(0.0, 0.0, 0.0, 0.0)
 
 func _update_player_camera():
 	if Engine.is_editor_hint():
@@ -69,17 +63,6 @@ func _update_level_bounds():
 	var tilemap = get_node_or_null("Terrain")
 	if !tilemap:
 		return
-	
-		# Clear existing ColorRect (if any)
-	for child in tilemap.get_children():
-		if child is ColorRect:
-			child.queue_free()
-
-	level_background = ColorRect.new()
-	level_background.color = background_color_light
-	level_background.size = Vector2(
-		width_in_tiles * tilemap.tile_set.tile_size.x,
-		height_in_tiles * tilemap.tile_set.tile_size.y
-	)
-	tilemap.add_child(level_background)
-	level_background.z_index = -1  # Render behind tiles
+		
+	tilemap.width_in_tiles = width_in_tiles
+	tilemap.height_in_tiles = height_in_tiles
--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -1,3 +1,4 @@
+@tool
 extends TileMapLayer
 
 var tile_size = tile_set.tile_size
@@ -5,22 +6,50 @@ var window_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 var window_height = ProjectSettings.get_setting("display/window/size/viewport_height")
 var configured_size = Vector2(window_width, window_height)
 
-var grid_dimensions 
+@export var background_color := Color(1.0, 1.0, 1.0, 1.0):
+	set(value):
+		background_color = value
+		_update_background_color()
+
+@export var width_in_tiles := 32:
+	set(value):
+		width_in_tiles = value
+		_update_level_bounds()
+
+@export var height_in_tiles := 64:
+	set(value):
+		height_in_tiles = value
+		_update_level_bounds()
+
+@onready var level_background = $LevelBackground
 
 func _ready():
-	var level = get_parent() as Node2D
-	grid_dimensions = Vector2i(level.width_in_tiles, level.height_in_tiles)
 	generate_border()
 
 func generate_border():
-	for x in range(grid_dimensions.x):
+	for x in range(width_in_tiles):
 		# Bottom border
-		set_cell(Vector2i(x, grid_dimensions.y - 1), 1, Vector2i(1, 2), 0)
+		set_cell(Vector2i(x, height_in_tiles - 1), 1, Vector2i(1, 2), 0)
 		# Top border
 		set_cell(Vector2i(x, 0), 1, Vector2i(1, 2), 0)
 
-	for y in range(1, grid_dimensions.y):
+	for y in range(1, height_in_tiles):
 		# Left border
 		set_cell(Vector2i(0, y), 1, Vector2i(1, 2), 0)
 		# Right border
-		set_cell(Vector2i(grid_dimensions.x - 1, y), 1, Vector2i(1, 2), 0)
+		set_cell(Vector2i(width_in_tiles - 1, y), 1, Vector2i(1, 2), 0)
+
+func _update_background_color():
+	if not level_background:
+		return
+
+	level_background.color = background_color
+
+func _update_level_bounds():
+	if not level_background:
+		return
+
+	level_background.size = Vector2(
+		width_in_tiles * tile_set.tile_size.x,
+		height_in_tiles * tile_set.tile_size.y
+	)
