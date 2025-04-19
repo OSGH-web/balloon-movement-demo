5ff658f486a7890926ea7ce1e91e32dd4a0a4ec5
Ben Taussig <btaussig97@gmail.com>
2025-04-19 16:55:40 -0400

    remove unnecessary variables from terrain.gd

--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -15,22 +15,25 @@ extends Node2D
 
 func _ready():
 	_update_level_bounds()
+	_generate_level_border()
 	_update_player_camera()
-
-func _update_player_camera():
-	if Engine.is_editor_hint():
-		return; # only run in the game
-	if $Player:
-		$Player._setup_camera_limits(width_in_tiles * 8 * $Player.physics_scale_factor, height_in_tiles * 8 * $Player.physics_scale_factor)
 
 func _update_level_bounds():
 	var tilemap = get_node_or_null("Terrain")
 	if !tilemap:
 		return
 
-	# needed for border generation
-	tilemap.width_in_tiles = width_in_tiles
-	tilemap.height_in_tiles = height_in_tiles
-
 	%TerrainBackground.size.x = width_in_tiles * 24
 	%TerrainBackground.size.y = height_in_tiles * 24
+
+func _generate_level_border():
+	if Engine.is_editor_hint():
+		return; # only run in the game
+
+	$"Terrain".generate_border(width_in_tiles, height_in_tiles)
+
+func _update_player_camera():
+	if Engine.is_editor_hint():
+		return; # only run in the game
+	if $Player:
+		$Player._setup_camera_limits(width_in_tiles * 8 * $Player.physics_scale_factor, height_in_tiles * 8 * $Player.physics_scale_factor)
--- scripts/terrain.gd
+++ scripts/terrain.gd
@@ -1,22 +1,6 @@
-@tool
 extends TileMapLayer
 
-
-@export var width_in_tiles := 32:
-	set(value):
-		width_in_tiles = value
-
-@export var height_in_tiles := 64:
-	set(value):
-		height_in_tiles = value
-
-func _ready():
-	generate_border()
-
-func generate_border():
-	if Engine.is_editor_hint():
-		return; # only run in the game
-
+func generate_border(width_in_tiles, height_in_tiles):
 	for x in range(width_in_tiles):
 		# Bottom border
 		set_cell(Vector2i(x, height_in_tiles - 1), 1, Vector2i(1, 2), 0)
@@ -28,4 +12,3 @@ func generate_border():
 		set_cell(Vector2i(0, y), 1, Vector2i(1, 2), 0)
 		# Right border
 		set_cell(Vector2i(width_in_tiles - 1, y), 1, Vector2i(1, 2), 0)
-		
