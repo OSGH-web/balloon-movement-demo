1d90a23464678e460ee83c7aa974b0eedc17417b
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-04-19 18:57:13 -0400

    clean up hud.gd

--- scripts/UI/hud.gd
+++ scripts/UI/hud.gd
@@ -20,8 +20,6 @@ func _process(delta: float) -> void:
 func _format_seconds(time : float) -> String:
 	var minutes := time / 60
 	var seconds := fmod(time, 60)
-
-	#return "%02d:%02d" % [minutes, seconds]
 	var milliseconds := fmod(time, 1) * 100
 
 	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
