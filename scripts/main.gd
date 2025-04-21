3096e59196458673669966e52340839e66ba42ca
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-04-21 16:06:37 -0400

    create start screen

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,7 @@
 # Empty main scene for flexibility later on. If we want an intro cutscene/main menu for example.
 extends Node
 
-func _ready():
-	GameManager.reset()
-	GameManager.load_next_level()
+func _input(event):
+	if event.is_action_pressed("ui_a"):
+		GameManager.reset()
+		GameManager.load_next_level()
