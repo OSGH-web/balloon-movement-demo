b48939985cf9027656956df86bff179a66f73372
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-04-24 18:00:09 -0400

    fix pause menu, add `ui_pause` input

--- scripts/UI/pause_menu.gd
+++ scripts/UI/pause_menu.gd
@@ -5,8 +5,8 @@ func _ready() -> void:
 		print("warning: parent is not pausable")
 	init_mute_audio_button()
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_ESCAPE):
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_pause"):
 		toggle_pause()
 
 func toggle_pause():
