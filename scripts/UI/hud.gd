df12b5bb4b3b18084c01291dd5acad49b5f551ab
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-04-28 21:43:18 -0400

    format all times the same way

--- scripts/UI/hud.gd
+++ scripts/UI/hud.gd
@@ -19,11 +19,4 @@ func _process(delta: float) -> void:
 	if GameManager.gameMode == GameManager.GameModes.ARCADE:
 		%Lives.text = "Lives: %d" % GameManager.lives
 		%Score.text = "Score: %d" % GameManager.score
-	%Timer.text = _format_seconds(GameManager.time)
-
-func _format_seconds(time : float) -> String:
-	var minutes := time / 60
-	var seconds := fmod(time, 60)
-	var milliseconds := fmod(time, 1) * 100
-
-	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
+	%Timer.text = GameManager.format_seconds(GameManager.time)
