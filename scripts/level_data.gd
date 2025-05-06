2de49243dc79d7947538df94e5bf121d366e4b3e
jaketaussig <jaketaussig@gmail.com>
2025-05-05 17:03:19 -0700

    set new dev times

--- scripts/level_data.gd
+++ scripts/level_data.gd
@@ -5,6 +5,6 @@ class_name LevelData
 # Gets populated upon time trial level clear
 @export var level_times = {}
 # hard-coded developer best times for time trail levels. 
-const dev_times = [9.68, 9.46, 4.65, 5.13, 25.71, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00]
+const dev_times = [9.68, 9.47, 4.55, 5.06, 23.85, 19.25, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00, 600.00]
 @export var high_score = 0
 @export var arcade_time = null
