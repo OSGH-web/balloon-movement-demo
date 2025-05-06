6f86683ff75737665e9db5d2688e00a7d46605bf
OSGH-web <btaussig97+open-source-github@gmail.com>
2025-05-06 23:04:41 +0000

    Deploying to gh-pages from @ OSGH-web/balloon-movement-demo@914070c6ddc29b24a65685f1f32cd229484425f5 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746572510|5810395';
+const CACHE_VERSION = '1746572671|5629319';
 /** @type {string} */
 const CACHE_PREFIX = 'Smokin My Laser-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
