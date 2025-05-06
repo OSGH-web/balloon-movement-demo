63e3af468e0e6d6d0d46ce9a7379bf1c8fa17d33
Ben Taussig <btaussig97@gmail.com>
2025-05-06 16:17:30 -0400

    add type annotation (`Array[int]`) to dev_times

--- scripts/level_data.gd
+++ scripts/level_data.gd
@@ -5,6 +5,6 @@ class_name LevelData
 # Gets populated upon time trial level clear
 @export var level_times = {}
 # hard-coded developer best times for time trail levels. 
-const dev_times = [9680, 9470, 4550, 5060, 23850, 19250, 15220, 17490, 19320, 11160, 28850, 12800, 5050, 17920, 38380]
+const dev_times: Array[int] = [9680, 9470, 4550, 5058, 23850, 19250, 15220, 17490, 19320, 11160, 28850, 12800, 5050, 17920, 38380]
 @export var high_score = 0
 @export var arcade_time = null
