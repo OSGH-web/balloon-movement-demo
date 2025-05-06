44447c297daa709699301e71415094f76dbcdd6f
Ben Taussig <btaussig97@gmail.com>
2025-05-06 15:40:36 -0400

    convert dev times to milliseconds

--- scripts/level_data.gd
+++ scripts/level_data.gd
@@ -5,6 +5,6 @@ class_name LevelData
 # Gets populated upon time trial level clear
 @export var level_times = {}
 # hard-coded developer best times for time trail levels. 
-const dev_times = [9680, 9470, 4550, 5060, 23850, 19250, 600000, 17490, 19320, 600000, 600000, 600000, 600000, 600000, 600000]
+const dev_times = [9680, 9470, 4550, 5060, 23850, 19250, 15220, 17490, 19320, 11160, 28850, 12800, 5050, 17920, 38380]
 @export var high_score = 0
 @export var arcade_time = null
