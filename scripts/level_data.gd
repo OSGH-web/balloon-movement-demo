11538b218d5a2a4db02656c2abac1b33ddb52615
jaketaussig <jaketaussig@gmail.com>
2025-05-08 16:45:50 -0700

    Added level goal times

--- scripts/level_data.gd
+++ scripts/level_data.gd
@@ -6,5 +6,6 @@ class_name LevelData
 @export var level_times = {}
 # hard-coded developer best times for time trail levels. 
 const dev_times: Array[int] = [9566, 9150, 4500, 5050, 21215, 16831, 14483, 14216, 15899, 10199, 28750, 11699, 4799, 16682, 31348]
+const goal_times: Array[int] = [12000, 11500, 5750, 6300, 26500, 21000, 18000, 17777, 20000, 12750, 35000, 14500, 7500, 20000, 40000]
 @export var high_score = 0
 @export var arcade_time = null
