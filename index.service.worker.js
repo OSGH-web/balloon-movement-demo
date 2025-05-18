59dbd1e97887eac34f2bc7dbeea84e93a6b85c0d
OSGH-web <OSGH-web@users.noreply.github.com>
2025-05-18 03:38:55 +0000

    Deploying to gh-pages from @ OSGH-web/beam-me-up@29fc6fabcb09ae1fd3c5fe97740b988c2ec7f2a6 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746747268|6107305';
+const CACHE_VERSION = '1747539521|6141472';
 /** @type {string} */
 const CACHE_PREFIX = 'Smokin My Laser-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
